3711059a8ec236a932b913f7d349ffc86f3c31a5
Dmitriu Ydin <da-ekb@mail.ru>
2024-01-23 11:43:07 +0500

    Add Sem_2_Task_2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine("Пример выполнения метода типа void");
+void NumToQuad(int num)
+{
+    Console.WriteLine(num*num);
+}
+
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NumToQuad(number);
+*/
+
+/*
+Console.WriteLine("Пример выполнения метода типа int");
+int NumToQuad(int num)
+{
+    return num*num;
+}
+
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumToQuad(number));
+*/
+
+Console.WriteLine("Задача 1");
+// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+// void deleteSecondDigit(int num)
+// {
+//     if(num >= 100 && num <= 999)
+//     {
+//         Console.WriteLine($"{num} -> {num/100 * 10 + num % 10}");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Введено не трёхзначное число");
+//     }
+// }
+
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// deleteSecondDigit(number);
+
+Console.WriteLine("Задача 2");
+
+// Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+void deleteSecondDigit(int num)
+{
+    if(num >= 100 && num <= 999)
+    {
+        Console.WriteLine($"{num} -> {num/100 * 10 + num % 10}");
+    }
+    else
+    {
+        Console.WriteLine("Введено не трёхзначное число");
+    }
+}
+
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+deleteSecondDigit(number);
